843419c98c9b26642dd3b9c8011ada068688795b
Revolution5 <damonboone99@gmail.com>
2022-07-07 15:00:22 -0700

    Add practical sections on button click

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { GeneralInfo } from "./components/general-info";
 import "./styles/app.css";
-import {Education} from "./components/education";
+import { Education } from "./components/education";
+import { Practical } from "./components/practical";
 
 export class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      eduSections: []
+      eduSections: [],
+      practicalSections: []
     }
   }
 
   addEdu = () => {
     this.setState({
       eduSections: [...this.state.eduSections, <Education/>]
+    })
+  }
+
+  addPractical = () => {
+    this.setState({
+      practicalSections:[...this.state.practicalSections, <Practical/>]
     })
   }
 
@@ -30,7 +38,8 @@ export class App extends Component {
           <button onClick={this.addEdu}>Add</button>
 
           <h2>Practical Experience</h2>
-          <button>Add</button>
+          {this.state.practicalSections}
+          <button onClick={this.addPractical}>Add</button>
       </div>
     )
   }
---
+++ src/components/practical.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+
+export class Practical extends Component {
+    render() {
+        return (
+          <form>
+              <div className="input">
+                  <label>Company Name</label>
+                  <input type="text"></input>
+              </div>
+              <div className="input">
+                    <label>Position Title</label>
+                    <input type="text"></input>
+              </div>
+              <div className="input">
+                    <label>Start Date</label>
+                    <input type="date"></input>
+              </div>
+              <div className="input">
+                    <label>End Date</label>
+                    <input type="date"></input>
+              </div>
+          </form>
+        )
+    }
+}
